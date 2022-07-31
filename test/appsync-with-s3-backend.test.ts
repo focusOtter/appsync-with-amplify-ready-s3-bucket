29f9d9e472783d111564c7ee7e1292ab8b50bb54
Michael Liendo <mtliendo@gmail.com>
2022-07-31 13:17:44 -0500

    Initial commit

---
+++ test/appsync-with-s3-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AppsyncWithS3Backend from '../lib/appsync-with-s3-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-with-s3-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AppsyncWithS3Backend.AppsyncWithS3BackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
